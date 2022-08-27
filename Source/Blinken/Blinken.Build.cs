ad13bb1b9cbbd3c425fce1abb3b2ff89e7e79f43
Bobby Albert <blood-pawwerewolf@outlook.com>
2022-08-26 21:57:26 -0500

    updated to UE5

--- Source/Blinken/Blinken.Build.cs
+++ Source/Blinken/Blinken.Build.cs
@@ -3,18 +3,16 @@
 using UnrealBuildTool;
 using System.IO;
 
-public class Blinken : ModuleRules
+ public class Blinken : ModuleRules
 {
 	public Blinken(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PublicDependencyModuleNames.AddRange( new string[] { "Engine", "Core", "InputCore", "CoreUObject" } ); 
 
-		if(Target.Platform == UnrealTargetPlatform.Win32 || Target.Platform == UnrealTargetPlatform.Win64)
+		if(Target.Platform == UnrealTargetPlatform.Win64)
 		{
 			// AlienFX SDK
-			PublicIncludePaths.Add( Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/AlienFXSDK/includes") ) );
-            RuntimeDependencies.Add(
-				new RuntimeDependency(Path.Combine(ModuleDirectory, "../../ThirdParty/AlienFXSDK/dll", Target.Platform.ToString(), "LightFX.dll"))
+			PublicIncludePaths.Add( Path.GetFullPath(Path.Combine(ModuleDirectory, "../../ThirdParty/AlienFXSDK/includes") )
 				);
 		}
 	}
